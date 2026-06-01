bb1c60c52b6615406461645b4d12bb9e5a0aa195
mengfong <testbbemf@gmail.com>
2026-06-02 07:02:05 +0800

    feat: add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
 export default defineConfig({
+  base: '/expense-tracker/',
   plugins: [preact()],
   test: {
     environment: 'happy-dom',
